1f36da3742f52ae536ea5228f4a470ec5f82dcb4
theussl (Stefan Theussl) <stefan.theussl@wu.ac.at>
2006-06-22 08:52:18 +0000

    bereinigte version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mgmtsc-wu@63 9c23517a-f912-0410-b08c-e6291725b2a9

--- study/edavis/edavis10.r
+++ study/edavis/edavis10.r
@@ -18,15 +18,16 @@ PP.opt <- PP.optimize.Huber("LDA",2,X,grp,cooling=0.999,r=1)
 PP.optimize.plot(PP.opt,X,grp)
 
 
-#plot(PP.opt,col=Seatbelts[,8]+1)
+## Visualisierung der Hauptkomponenten 1:2
 
-## Visualisierung der Hauptkomponenten
+pc <- princomp(X)
+biplot(pc$scores[,1:2],pc$loadings[,1:2])
+scatterplot.matrix(X)
 
 par(mfrow=c(1,2))
-pc <- princomp(X)
+
 hist(pc$sco[,1],freq=F, xlab="1. Hauptkomponente",ylab="",main="",cex.lab=1.2)
 lines(density(pc$sco[,1]))
-
 
 hist(pc$sco[,2],freq=F, xlab="2. Hauptkomponente",ylab="",main="",cex.lab=1.2)
 lines(density(pc$sco[,2]))
@@ -44,8 +45,7 @@ X2 <- rmvnorm(n, rep(1.5,p),diag(p:1)) %*% trans
 X <- scale(rbind(X1,X2))
 grp <- c(rep(1,n),rep(2,n))
 
-## Scatterplot
-pairs(X)
+
 
 ## Projection Pursuit
 
@@ -55,7 +55,7 @@ PP.optimize.plot(PP.opt,X,grp)
 
 # Man erkennt eine rechte und eine linke Gruppe
 
-## Visualisierung der Hauptkomponenten
+## Visualisierung der Hauptkomponenten 1:2
 par(mfrow=c(1,2))
 pc <- princomp(X)
 hist(pc$sco[,1],freq=F, xlab="1. Hauptkomponente",ylab="",main="",cex.lab=1.2)
